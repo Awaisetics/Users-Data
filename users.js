2ad05a51ae00df950350dadcddfa0182df1c9a73
Muhammad Awais Danish <mr.awaisdanish@gmail.com>
2022-07-27 21:26:51 +0500

    ..

--- users.js
+++ users.js
@@ -15,20 +15,22 @@ class Users {
     }
 
     constructHTML(data) {
-        console.log(data);
-        const usersHtml = data.map((user) => {
-            return `
-                <tr id='u-${user.id}'>
-                    <th scope="row">${user.id}</th>
-                    <td>${user.name}</td>   
-                    <td>${user.email}</td>
-                    <td>${user.address.city}</td>
-                    <td><a href="https://${user.website}" target="_blank">${user.website}</a></td>
-                    <td><button onclick='user.delete(${user.id})' class="btn btn-sm btn-danger">Delete</button></td>
-                </tr>
-            `;
-        }).join('');
-        this.list.innerHTML = usersHtml;
+        if (data) {
+            const usersHtml = data.map((user) => {
+                return `
+                    <tr id='u-${user.id}'>
+                        <th scope="row">${user.id}</th>
+                        <td>${user.name}</td>   
+                        <td>${user.email}</td>
+                        <td>${user.address.city}</td>
+                        <td><a href="https://${user.website}" target="_blank">${user.website}</a></td>
+                        <td><button onclick='user.delete(${user.id})' class="btn btn-sm btn-danger">Delete</button></td>
+                    </tr>
+                `;
+            }).join('');
+            this.list.innerHTML = usersHtml;
+        }
+
     }
 
     delete (uid) {
